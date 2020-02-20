3b42db125e40bbc1a0549bfa0b6278e824896f22
Matheus-Godinho <matheus.brc22@gmail.com>
2020-02-20 17:23:08 -0300

    Entities implemented and tested

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,50 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.Installment;
+
 public class Program {
+	
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Contract contract;
+			int number;
+			Date date;
+			double totalValue;
+			
+		try {
+			number = sc.nextInt();
+			date = sdf.parse(sc.next());
+			totalValue = sc.nextDouble();
+			contract = new Contract(number, date, totalValue);
+			
+			contract.addInstallments(new Installment(sdf.parse("25/07/2018"), 206.04));
+			contract.addInstallments(new Installment(sdf.parse("25/08/2018"), 208.08));
+			contract.addInstallments(new Installment(sdf.parse("25/09/2018"), 210.12));
+			
+			for (Installment i: contract.getInstallments())
+				System.out.printf("%s%n", i.toString());
+			
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+		catch (RuntimeException e) {
+			e.printStackTrace();
+		}
+		
+		sc.close();
 
 	}
 
--- src/model/entities/Installment.java
+++ src/model/entities/Installment.java
@@ -1,5 +1,39 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Installment {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private Date date;
+	private Double value;
+	
+	public Installment() {
+	}
+	public Installment(Date date, Double value) {
+		this.date = date;
+		this.value = value;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	@Override
+	public String toString() {
+		return sdf.format(getDate()) + " - " + String.format("%.2f", getValue());
+	}
 
 }
